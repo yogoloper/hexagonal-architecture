120e62a34bd8fb05d9db6255935b927ec7d726d5
yogoloper <jguddnjs@gmail.com>
2025-07-10 23:08:01 +0900

    feat: member 도메인 모델 확장

--- src/main/java/tobyspring/splearn/domain/Member.java
+++ src/main/java/tobyspring/splearn/domain/Member.java
@@ -18,11 +18,15 @@ public class Member {
 
     private MemberStatus status;
 
-    public Member(String email, String nickname, String passwordHash) {
+    private Member(String email, String nickname, String passwordHash) {
         this.email = Objects.requireNonNull(email);
         this.nickname = Objects.requireNonNull(nickname);
         this.passwordHash = Objects.requireNonNull(passwordHash);
         this.status = MemberStatus.PENDING;
+    }
+
+    public static Member create(String email, String nickname, String password, PasswordEncoder passwordEncoder) {
+        return new Member(email, nickname, passwordEncoder.encode(password));
     }
 
     public void avtivate() {
@@ -35,5 +39,17 @@ public class Member {
         state(status == MemberStatus.ACTIVE, "ACTIVE 상태가 아닙니다");
 
         this.status = MemberStatus.DEACTIVATED;
+    }
+
+    public boolean verifyPassword(String password, PasswordEncoder passwordEncoder) {
+        return passwordEncoder.matches(password, this.passwordHash);
+    }
+
+    public void changeNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void changePassword(String password, PasswordEncoder passwordEncoder) {
+        this.passwordHash = passwordEncoder.encode(password);
     }
 }
--- src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -1,27 +1,44 @@
 package tobyspring.splearn.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberTest {
+    Member member;
+    PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        this.passwordEncoder = new PasswordEncoder() {
+            @Override
+            public String encode(String password) {
+                return password.toUpperCase();
+            }
+
+            @Override
+            public boolean matches(String password, String passwordHash) {
+                return encode(password).equals(passwordHash);
+            }
+        };
+
+        member = Member.create("toby@splearn.app", "Toby", "secret", passwordEncoder);
+    }
+
     @Test
     void createMember() {
-        var member = new Member("toby@splearn.app", "Toby", "secret");
-
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
     }
 
-    @Test
-    void constructorNullCheck() {
-        assertThatThrownBy(() -> new Member(null, "Toby", "secret")).isInstanceOf(NullPointerException.class);
-    }
+//    @Test
+//    void constructorNullCheck() {
+//        assertThatThrownBy(() -> Member.create(null, "Toby", "secret", null)).isInstanceOf(NullPointerException.class);
+//    }
 
     @Test
     void activate() {
-        var member = new Member("toby@splearn.app", "Toby", "secret");
-
         member.avtivate();
 
         assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
@@ -29,8 +46,6 @@ class MemberTest {
 
     @Test
     void activateFail() {
-        var member = new Member("toby@splearn.app", "Toby", "secret");
-
         member.avtivate();
 
         assertThatThrownBy(() -> {
@@ -40,7 +55,6 @@ class MemberTest {
 
     @Test
     void deactivate() {
-        var member = new Member("toby@splearn.app", "Toby", "secret");
         member.avtivate();
 
         member.deavtivate();
@@ -51,13 +65,34 @@ class MemberTest {
 
     @Test
     void deactivateFail() {
-        var member = new Member("toby@splearn.app", "Toby", "secret");
-
         assertThatThrownBy(member::deavtivate).isInstanceOf(IllegalStateException.class);
 
         member.avtivate();
         member.deavtivate();
 
         assertThatThrownBy(member::deavtivate).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void verifyPassword() {
+        assertThat(member.verifyPassword("secret", passwordEncoder)).isTrue();
+        assertThat(member.verifyPassword("hello", passwordEncoder)).isFalse();
+    }
+
+    @Test
+    void changeNickname() {
+        assertThat(member.getNickname()).isEqualTo("Toby");
+
+        member.changeNickname("Charlie");
+
+        assertThat(member.getNickname()).isEqualTo("Charlie");
+    }
+
+    @Test
+    void changePassword() {
+        member.changePassword("verysecret", passwordEncoder);
+
+        assertThat(member.verifyPassword("verysecret", passwordEncoder)).isTrue();
+        
     }
 }
